6cdd4f8949f30d45112d9ba2cdbfc071ff77ed78
Luigi Thirty <luigi30@gmail.com>
2020-02-02 02:14:47 -0600

    Wooooo

--- test.asm
+++ test.asm
@@ -124,33 +124,122 @@ CheckCmdLength:
 	; If so, the argument starts at buffer_Input+2.
 	ld		a,(MON_Command+1)
 	cp		$20
-	jr		nz,GetArgument
+	jr		nz,GotCmdLength
 	ld		a,2
 	ld		(MON_ArgStartsAt),a
 
-GetArgument:
-#local
-	; Copy 10 characters from buffer_Input+MON_ArgStartsAt into MON_Argument
+GotCmdLength:
 	ld		b,10
-	ld		hl,buffer_Input
-	ld		de,MON_Argument
+	call	GetArgument
+	call	CmdDebugOutput
 
-	ld		a,(MON_ArgStartsAt)
-	ld		c,a
-	ld		b,0		; BC is now ArgStartsAt
-	add		hl,bc
-	
-	; HL is now the beginning of the argument.
-	; DE is now the destination address.
-ArgumentCopyLoop:
-	ld		a,(hl)
-	ld		(de),a
-	inc		hl
-	inc		de
-	djnz	ArgumentCopyLoop
-#endlocal
+	; debug: preload some values
+	ld		hl,$0200
+	ld		(MemoryOutputStartAddr),hl
+	ld		hl,$023F
+	ld		(MemoryOutputEndAddr),hl
+	call	Monitor_DoMemoryOutput
 
-	; Debug output
+; Loop...
+InputLoopEnd:
+	ld		de,strCRLF
+	ld		c,B_STROUT
+	DoBIOS
+	ld		de,strCRLF
+	ld		c,B_STROUT
+	DoBIOS
+	jp		GetInputString
+
+;;;;;;;;
+ConvertHex16ToString:
+	; Convert the value in HexToString_Source to ASCII characters.
+	ld		ix,HexToString_Source
+	ld		iy,HexToString_Dest
+
+	; A
+	ld		a,(ix+1)
+	and		$F0		; now we only have A
+	srl		a
+	srl		a
+	srl		a
+	srl		a
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+0),a
+
+	; B
+	ld		a,(ix+1)
+	and		$0F		; now we only have B
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+1),a
+
+	; C
+	ld		a,(ix)
+	and		$F0		; now we only have C
+	srl		a	
+	srl		a
+	srl 	a
+	srl 	a
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+2),a
+
+	; D
+	ld		a,(ix)
+	and		$0F		; now we only have D
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+3),a
+
+	ld		a,0
+	ld		(iy+4),a
+
+	ret
+
+;;;
+ConvertHex8ToString:
+	; Convert the value in HexToString_Source to ASCII characters.
+	; A
+	ld		iy,HexToString_Dest
+	ld		ix,HexToString_Source
+
+	ld		a,(ix)
+	and		$F0		; now we only have A
+	srl		a	
+	srl		a
+	srl 	a
+	srl 	a
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+0),a
+
+	; B
+	ld		a,(ix)
+	and		$0F		; now we only have B
+	add  	a,$90
+	daa
+	adc  	a,$40
+	daa
+	ld		(iy+1),a
+
+	ld		a,0
+	ld		(iy+2),a
+
+	ret
+
+;;;;;;;;
+CmdDebugOutput:
 	ld		de,strDbgCmd
 	ld		c,B_STROUT
 	DoBIOS
@@ -175,15 +264,36 @@ ArgumentCopyLoop:
 	ld		c,B_STROUT
 	DoBIOS
 
-; Loop...
-InputLoopEnd:
-	jp		GetInputString
+	ret
+
+;;;;;;;;;;;;;;;;;;
+GetArgument:
+#local
+	; Copy B characters from buffer_Input+MON_ArgStartsAt into MON_Argument
+	ld		hl,buffer_Input
+	ld		de,MON_Argument
+
+	ld		a,(MON_ArgStartsAt)
+	ld		c,a
+	ld		b,0		; BC is now ArgStartsAt
+	add		hl,bc
+	
+	; HL is now the beginning of the argument.
+	; DE is now the destination address.
+ArgumentCopyLoop:
+	ld		a,(hl)
+	ld		(de),a
+	inc		hl
+	inc		de
+	djnz	ArgumentCopyLoop
+
+	ret
+#endlocal
 
 ;;
 ;;
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ClearInputBuffer:
 	ld		a,0
 	ld		hl,buffer_Input
@@ -197,12 +307,123 @@ ClearInputBuffer:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+Monitor_DoMemoryLabel:
+	; Formatting: start address
+	ld		hl,(MemoryOutputCurAddr)
+	ld		(HexToString_Source),hl
+	call	ConvertHex16ToString
+	ld		de,HexToString_Dest
+	ld		c,B_STROUT
+	DoBIOS
+	ld		e,":"
+	ld		c,B_CONOUT
+	DoBIOS
+	ld		e," "
+	ld		c,B_CONOUT
+	DoBIOS
+	ret
+
+Monitor_PrintBytes:
+	; Input:
+	; B  - how many bytes to dump
+	; IX - pointer to start of memory area to dump
+	push	bc
+
+	ld		a,(ix)						; A has a memory byte
+	ld		(HexToString_Source),a		
+	push	ix
+	call	ConvertHex8ToString			; Convert it to ASCII
+
+	; Print two characters of output and a space
+	ld		a,(HexToString_Dest)
+	ld		e,a
+	ld		c,B_CONOUT
+	DoBIOS								
+	ld		a,(HexToString_Dest+1)
+	ld		e,a
+	ld		c,B_CONOUT
+	DoBIOS
+	ld		e," "
+	ld		c,B_CONOUT
+	DoBIOS
+
+	; Advance the memory source pointer. Continue until B == 0.
+	pop		ix
+	inc		ix
+
+	pop		bc
+	djnz	Monitor_PrintBytes
+
+	ret
+
+Monitor_DoMemoryOutput:
+#local
+	ld		hl,(MemoryOutputStartAddr)
+	ld		(MemoryOutputCurAddr),hl
+
+	call	Monitor_DoMemoryLabel
+
+	; Output 16 memory bytes
+	ld		hl,(MemoryOutputEndAddr)
+	ld		bc,(MemoryOutputStartAddr)
+	scf
+	ccf		; Clear carry flag to get the proper subtraction result.
+	inc		hl	
+	sbc		hl,bc
+	ld		(MemoryOutputBytesLeft),hl
+
+	ld		ix,(MemoryOutputStartAddr)
+	ld		b,16
+	call	Monitor_PrintBytes
+
+EndMemoryLine:
+	ld		de,strCRLF
+	ld		c,B_STROUT
+	DoBIOS
+
+	ld		hl,(MemoryOutputBytesLeft)
+	scf
+	ld		bc,16
+	sbc		hl,bc	; Subtract the 16 bytes we already read.
+	jp		m,Done	; End if we're out of memory to write.
+	jp		z,Done	; End if we're out of memory to write.
+	ld		(MemoryOutputBytesLeft),hl
+
+	ld		bc,16
+	ld		hl,(MemoryOutputCurAddr)
+	add		hl,bc						; Advance start pointer
+	ld		(MemoryOutputCurAddr),hl	
+	call	Monitor_DoMemoryLabel
+
+	ld		b,16						; Another 16 bytes
+	ld		ix,(MemoryOutputCurAddr)
+	call	Monitor_PrintBytes
+	jp		EndMemoryLine
+
+Done:
+	ret
+#endlocal
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 #data DATA,0x8000,0x8000	; Data section in RAM. Assume 32K of RAM on an RC2014.
 
 buffer_base:
 buffer_len:				.db 0
 buffer_inputsize:		.db 0
 buffer_Input:			.ds	255	; 255 bytes of input storage
+
+;;;
+StringToHex_Source:		.ds 16
+StringToHex_Dest:		.ds 8
+
+HexToString_Source:		.ds	4
+HexToString_Dest:		.ds 4
+
+;;;
+MemoryOutputStartAddr:	.dw 0
+MemoryOutputCurAddr:	.dw 0
+MemoryOutputEndAddr:	.dw 0
+MemoryOutputBytesLeft:	.dw 0
 
 ;;;;;;;;;;;;;;;;;;;
 ; ROM monitor data stuff
